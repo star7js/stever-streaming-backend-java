1fb9513e821120b719aa319d491dd2cbdb9a0906
Steven Baca <stevenbaca@Stevens-MacBook-Pro.local>
2025-05-02 17:04:28 -0600

    Removed unused Jackson dependency.

--- producer-simulator/build.gradle.kts
+++ producer-simulator/build.gradle.kts
@@ -10,9 +10,6 @@ java {
 dependencies {
     implementation("org.apache.kafka:kafka-clients:4.0.0")
     implementation("org.json:json:20250107")
-//    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
-//    implementation("com.fasterxml.jackson.core:jackson-core:2.17.0")
-//    implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.0")
 }
 
 application {
